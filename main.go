bf87fe5ee94a8b6cc2a93af3f27f673cbabbd75a
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-08-13 10:54:56 +0800

    Ch3.01: monoliths and decoupling

--- main.go
+++ main.go
@@ -12,10 +12,13 @@ type apiConfig struct {
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := apiConfig{}
-	mux.Handle("/app/*", apiCfg.middlewareMatricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /healthz", handlerHealthz)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("GET /reset", apiCfg.handlerReset)
+
+	fsHandler := apiCfg.middlewareMatricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/*", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerHealthz)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    "localhost:8080",
@@ -33,23 +36,4 @@ func handlerHealthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-}
-
-func (apiCfg *apiConfig) middlewareMatricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		apiCfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (apiCfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits)))
-}
-
-func (apiCfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	apiCfg.fileserverHits = 0
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Reset hits to 0"))
 }
---
+++ matrics.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charaset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) middlewareMatricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
